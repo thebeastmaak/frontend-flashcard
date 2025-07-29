f1d64963826a45bec97b2af2b9a2ae09a9892eb4
thebeastmaak <soufianemaakouk@gmail.com>
2025-07-29 22:09:52 +0200

    Create flashcard.js

---
+++ src/flashcard.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import './Flashcard.css';
+
+function Flashcard({ english, german }) {
+  const [flipped, setFlipped] = useState(false);
+
+  return (
+    <div className="card" onClick={() => setFlipped(!flipped)}>
+      <div className={`card-inner ${flipped ? 'flipped' : ''}`}>
+        <div className="card-front">{english}</div>
+        <div className="card-back">{german}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Flashcard;
