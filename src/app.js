089002fe04880ddca539f7c6a9ffd1507491144b
thebeastmaak <soufianemaakouk@gmail.com>
2025-07-29 22:10:29 +0200

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Flashcard from './Flashcard';
+import './App.css';
+
+const API_URL = 'https://<your-render-backend>.onrender.com/cards';
+
+function App() {
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    axios.get(API_URL)
+      .then(res => setCards(res.data))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <div className="container">
+      <h1>German Flashcards</h1>
+      <div className="card-grid">
+        {cards.map(card => (
+          <Flashcard key={card._id} english={card.english} german={card.german} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
