93f7b137dc4b795b0dd3aa3a06050f0ec908041f
thebeastmaak <soufianemaakouk@gmail.com>
2025-07-31 20:28:18 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';  // or './app' depending on your filename
+import App from './app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
