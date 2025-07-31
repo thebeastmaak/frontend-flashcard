6313f1fc1b286007e1d3d8a593c3f614bc9e5b0e
thebeastmaak <soufianemaakouk@gmail.com>
2025-07-31 20:27:51 +0200

    Update flashcard.js

--- src/flashcard.js
+++ src/flashcard.js
@@ -1,15 +1,13 @@
 import React, { useState } from 'react';
 import './flashcard.css';
 
-function Flashcard({ english, german }) {
+function Flashcard({ card }) {
   const [flipped, setFlipped] = useState(false);
 
   return (
-    <div className="card" onClick={() => setFlipped(!flipped)}>
-      <div className={`card-inner ${flipped ? 'flipped' : ''}`}>
-        <div className="card-front">{english}</div>
-        <div className="card-back">{german}</div>
-      </div>
+    <div className={`flashcard ${flipped ? 'flipped' : ''}`} onClick={() => setFlipped(!flipped)}>
+      <div className="front">{card.word}</div>
+      <div className="back">{card.translation}</div>
     </div>
   );
 }
